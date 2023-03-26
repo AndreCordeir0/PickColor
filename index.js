ac2a07053e1453845334b64a35f338bdd13a42e8
Your Name <you@example.com>
2023-03-25 21:23:28 -0300

    align center

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ function pick(event, destination) {
     const bounding = canvas.getBoundingClientRect();
     const x = event.clientX - bounding.left;
     const y = event.clientY - bounding.top;
+    console.log(bounding);
     const pixel = ctx.getImageData(x, y, 1, 1);
     const data = pixel.data;
     const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
